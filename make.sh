1ee5159be0c22e2296256b25d9228f0c583b4437
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2025-02-16 10:32:06 -0500

    update file name year

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-pdflatex AnkitKumar-CV-2024.tex
-bibtex AnkitKumar-CV-2024
-pdflatex AnkitKumar-CV-2024.tex
-pdflatex AnkitKumar-CV-2024.tex
+pdflatex AnkitKumar-CV-2025.tex
+bibtex AnkitKumar-CV-2025
+pdflatex AnkitKumar-CV-2025.tex
+pdflatex AnkitKumar-CV-2025.tex
